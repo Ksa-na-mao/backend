30418870fab2d3ad77edd70ea5c7c469c7d78b35
MiguelMDutra <miguelnewgen@gmail.com>
2026-09-09 11:14:48 -0300

    adicionando um banco pros testes

--- backend/src/database/models/index.ts
+++ backend/src/database/models/index.ts
@@ -15,13 +15,18 @@ import PreparationHistory from "./preparationhistory";
 import Notification from "./Notification";
 import Like from "./Like";
 
+const database =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_DB_DATABASE
+    : process.env.DB_DATABASE;
+
 const sequelize = new Sequelize({
   dialect: "postgres",
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: database,
 });
 
 const CommentModel = Comment(sequelize);
