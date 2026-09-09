f574f61b154f7c59a902ee9b96912ac3f7c02b4c
MiguelMDutra <miguelnewgen@gmail.com>
2026-09-09 12:34:42 -0300

    Linkando o swagger na api

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,9 +3,13 @@ import mainRoutes from "./core/routes/Routes.js";
 import ErrorHandler from "./core/Errors/1ErrorHandler.js";
 import Error404Middleware from "./core/Errors/Error404Middleware.js";
 
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./swagger/swagger.js";
+
 const app = express();
 
 mainRoutes(app);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(Error404Middleware);
 app.use(ErrorHandler);
 
