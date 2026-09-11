cdbffbaed353f704edadeac9cd0fe986092953c1
MiguelMDutra <miguelnewgen@gmail.com>
2026-09-11 13:58:38 -0300

    Vendo se o usuário está cadastrado ou não

--- backend/src/core/jwt/verifyAccount.ts
+++ backend/src/core/jwt/verifyAccount.ts
@@ -1,8 +1,11 @@
 import Unauthorized from "../Errors/Unauthorized.ts";
 import verifyToken from "./verifyToken.js";
 import { Request, Response, NextFunction } from "express";
+import UserServices from "@/modules/user/User.Services.ts";
+import Forbidden from "../Errors/Forbidden.ts";
+const userServices = new UserServices();
 
-function verifyAccount(req: Request, res: Response, next: NextFunction) {
+async function verifyAccount(req: Request, res: Response, next: NextFunction) {
   const header = req.headers.authorization;
   if (!header || !header.startsWith("Bearer ")) {
     return next(new Unauthorized());
@@ -11,9 +14,11 @@ function verifyAccount(req: Request, res: Response, next: NextFunction) {
   try {
     const user = verifyToken(token);
     req.user = user;
-    next();
+    const active = await userServices.getUserById(req.user.userId);
+    if (active) next();
+    else throw new Forbidden("Sua conta está desativada.");
   } catch (error) {
-    next(new Unauthorized());
+    next(error);
   }
 }
 
